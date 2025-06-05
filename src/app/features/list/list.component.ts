585bfafad2c7fa49d134713c3e62a468976f87fc
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-06-05 18:15:27 -0300

    feat: add card paramostrar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product-interface';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -16,6 +17,7 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
     this.productsService.getAll().subscribe({
       next: (products) => {
+        console.log(products);
         this.products = products;
       },
       error: (error) => {
