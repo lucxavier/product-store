fc52d00021cb811c0dfdca9fc048259fd37108ed
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-06-06 13:52:25 -0300

    feat: adiciona link para cadastrar um produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,10 +4,11 @@ import { Product } from '../../shared/interfaces/product-interface';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, MatButtonModule, RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
